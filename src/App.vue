c0e98679b02d4a298a813aa9163bee930a3f1802
David Bauer <DBauer15@users.noreply.github.com>
2020-12-29 11:23:30 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<!-- This file will be used to layout and connect your views -->
+<template>
+  <div id="app">
+      <!-- This is only a basic setup to display your views. Use HTML and CSS to create your layout using these basic commands -->
+      <View1 />
+      <View2 />
+  </div>
+</template>
+
+<script>
+import View1 from './components/View1.vue'
+import View2 from './components/View2.vue'
+
+export default {
+  name: 'App',
+  components: {
+    View1,
+    View2
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
